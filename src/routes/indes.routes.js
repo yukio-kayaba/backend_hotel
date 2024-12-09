cb8b22feaf9627b57e0b42b3d9b84ee221aecae8
yukio <zviamontevilca@gmail.com>
2024-12-09 17:44:39 -0500

    cambios
    se ha corregido el error del tokem

--- src/routes/indes.routes.js
+++ src/routes/indes.routes.js
@@ -1,6 +1,24 @@
 import { Router } from "express";
 import { pool } from "../basedatos.js";
 
+function tokem_validator(ip){
+    const userAgent = ip;
+    const compressedInfo = userAgent.match(/(Windows NT|Mac OS X|Linux|Android|iPhone).*?(Chrome|Firefox|Safari|Edge|Opera)\/([\d.]+)/);
+    
+    let tokem = `t${valor1}`;
+    if (compressedInfo) {
+        const os = compressedInfo[1]; 
+        const browser = compressedInfo[2]; 
+        const version = compressedInfo[3];
+
+        console.log(`Sistema operativo: ${os}`);
+        console.log(`Navegador: ${browser}`);
+        console.log(`Versión: ${version}`);
+        tokem += `adm${os}n${browser}v${version}`;
+        return tokem;
+    }
+}
+
 const router = Router();
 const convertidor_arrays_imagenes = (enlaces)=>{
     let datos = Array();
@@ -49,31 +67,17 @@ router.post('/add',async(req,res)=>{
 
 router.post('/login_adm',async(req,res)=>{
     try {
-        const {correo,contra,ip} = req.body;
+        const {correo,contra,ip,valor1} = req.body;
         // console.log(req.body);
         const exp_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const exp_contra = /^[a-zA-Z0-9_áéíóúÁÉÍÓÚñÑçÇ\s]+$/;
     
-        const valor = getRandomInRange(1,150);
-        const valor1 = getRandomInRange(1,150);
+        // const valor1 = getRandomInRange(1,150);
         console.log(`correo : ${correo}  - contra : ${contra} - ip : ${ip} `);
         if(!exp_contra.test(contra) || !exp_email.test(correo)){
             return res.send("error de expresion");
         }
-        const userAgent = ip;
-        const compressedInfo = userAgent.match(/(Windows NT|Mac OS X|Linux|Android|iPhone).*?(Chrome|Firefox|Safari|Edge|Opera)\/([\d.]+)/);
-        
-        let tokem = `t${valor1}`;
-        if (compressedInfo) {
-            const os = compressedInfo[1]; 
-            const browser = compressedInfo[2]; 
-            const version = compressedInfo[3];
-
-            console.log(`Sistema operativo: ${os}`);
-            console.log(`Navegador: ${browser}`);
-            console.log(`Versión: ${version}`);
-            tokem += `adm${os}n${browser}v${version}`;
-        }
+        let tokem = tokem_validator(valor1);
 
         const [rows] = await pool.query("call inicio_sesion_adm(?, ? , ?);",[correo,contra,tokem]);
         if(rows.length > 0){
@@ -93,7 +97,7 @@ router.post('/reporte_dias',async(req,res)=>{
     if(!exp_contra.test(token) || !exp_contra.test(id) || !exp_contra.test(id_token)){
         return res.send("token no valido");
     }
-
+    
     const [datos] = await pool.query(`select a.correo from administradores a, tokens_user t where a.id = ${id} and t.id_token = ${id_token} and t.token = '${token}' and t.activo = '1';`); 
 
     if(datos[0]){
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
-import { pool } from "../basedatos";
+import { pool } from "../basedatos.js";
 
 const Ruta = Router();
 
 Ruta.post('/', async(req,res) => {
     const {id_user,tokem,id_tokem} = req.headers;
     const {cantidad} = req.body;
+    const [datos] = await pool.query(`select a.correo from administradores a, tokens_user t where a.id = ${id_user} and t.id_token = ${id_tokem} and t.token = '${tokem}' and t.activo = '1';`); 
+    // console.log(`id_user ${id_user} id_tokem : ${id_tokem} tokem : ${tokem}`);
+    if(datos[0]){
+        const [rows] = await pool.query("select id_user,nombre,apellido,dni,fecha_creacion from usuarios_clientes;");
+        
+        // let datos_r = [JSON.stringify(rows[0]),JSON.stringify(hors[0])];
+        console.log(`datos _ r : ${JSON.stringify(rows)}`);
+        return res.json(rows);
+    }
     res.send("hola");
 }); 
 
